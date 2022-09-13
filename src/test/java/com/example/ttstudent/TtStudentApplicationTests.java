3757400ec6b838925c609fdd0affb00a77e5f7b5
CanhNguyen <leagueyasuo186@gmail.com>
2022-09-14 06:48:47 +0700

    k

---
+++ src/test/java/com/example/ttstudent/TtStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ttstudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtStudentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
